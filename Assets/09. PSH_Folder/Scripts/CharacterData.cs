ac2c492c3979577ebef74e8f2137fa88d4878234
xmasoo <xmasoo73@gmail.com>
2025-08-16 17:14:40 +0900

    [Test] 임시 캐릭터 SO 생성

---
+++ Assets/09. PSH_Folder/Scripts/CharacterData.cs
@@ -0,0 +1,36 @@
+using System.Numerics;
+using UnityEngine;
+
+public enum Rarity
+{
+    A,B,C
+}
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Test Character")]
+public class CharacterData : ScriptableObject
+{
+    [Header("캐릭터 정보")]
+    public string characterName;
+    public Sprite characterSprite;
+    [TextArea]
+    public string description;
+
+    [Header("가챠 정보")]
+    public Rarity rarity; // 이 캐릭터의 등급
+
+    [Header("능력치 (BigInteger)")]
+    [SerializeField]
+    private string maxHealthString = "100";
+    public BigInteger MaxHealth { get; private set; }
+
+    private void OnValidate()
+    {
+        // 인스펙터에서 입력한 체력(문자열)을 실제 BigInteger 값으로 변환합니다.
+        if (!BigInteger.TryParse(maxHealthString, out BigInteger parsedValue))
+        {
+            parsedValue = 100;
+        }
+        MaxHealth = parsedValue;
+        maxHealthString = MaxHealth.ToString();
+    }
+}
