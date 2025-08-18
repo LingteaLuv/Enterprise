9f9ed32ba845db89f6ebc7427e144bbebf174c69
Usanso <save1086@naver.com>
2025-08-18 15:12:35 +0900

    [Feat] 캐릭터 스크립터블 오브젝트 및 캐릭터 전투력 산정 스크립트 작성

---
+++ Assets/07. LHI_Folder/Scripts/CharacterData.cs
@@ -0,0 +1,78 @@
+using System;
+using UnityEngine;
+
+
+namespace LHI
+{
+    public enum Role
+    {
+        Captain,
+        Boatswain,
+        Sailor,
+        Cook
+    }
+
+    public enum Affiliation
+    {
+        navy,
+        pirate,
+        monster
+    }
+
+    [CreateAssetMenu(fileName = "CharacterData", menuName = "Scriptable Objects/CharacterData")]
+    public class CharacterData : ScriptableObject
+    {
+        [Header("캐릭터 정보")]
+        [Tooltip("캐릭터의 이름")]
+        public string characterName;
+        [Tooltip("캐릭터의 역할")]
+        public Role role;
+        [Tooltip("캐릭터의 소속")]
+        public Affiliation affiliation;
+
+        [Header("캐릭터 스탯")]
+        [Tooltip("캐릭터의 공격력")]
+        public float attack;
+        [Tooltip("캐릭터의 체력")]
+        public float health;
+        [Tooltip("캐릭터의 방어력")]
+        public float defense;
+
+        [Range(0, 100)]
+        [Tooltip("캐릭터의 치명타 확률 (%)")]
+        public float criticalChance;
+        [Tooltip("캐릭터의 치명타 배율 (%), ex)2배를 200로 작성할 것")]
+        public float criticalDamage;
+        [Tooltip("캐릭터가 자신의 턴을 가지는 속도를 결정하는 스탯")]
+        public float speed;
+
+        private void OnValidate()
+        {
+            if (attack < 0f)
+            {
+                attack = 0f;
+                Debug.Log($"[CharacterData.attack] '{attack}' 의 값이 잘못 되었습니다. (음수 X)");
+            }
+            if (health < 0f)
+            {
+                health = 0f;
+                Debug.Log($"[CharacterData.health] '{health}' 의 값이 잘못 되었습니다. (음수 X)");
+            }
+            if (defense < 0f)
+            {
+                defense = 0f;
+                Debug.Log($"[CharacterData.defense] '{defense}' 의 값이 잘못 되었습니다. (음수 X)");
+            }
+            if (criticalDamage < 0f)
+            {
+                criticalDamage = 0f;
+                Debug.Log($"[CharacterData.criticalDamage] '{criticalDamage}' 의 값이 잘못 되었습니다. (음수 X)");
+            }
+            if (speed < 0f)
+            {
+                speed = 0f;
+                Debug.Log($"[CharacterData.speed] '{speed}' 의 값이 잘못 되었습니다. (음수 X)");
+            }
+        }
+    }
+}
---
+++ Assets/07. LHI_Folder/Scripts/CombatPowerCalculation.cs
@@ -0,0 +1,159 @@
+using UnityEngine;
+
+namespace LHI
+{
+    public class CombatPowerCalculation : MonoBehaviour
+    {
+        // 역할에 따라 전투력 산정 방식이 다름
+        // CombatPower를 줄여서 CP로 명명
+        // 처음 AllCPCalculate 메서드로 모든 전투력을 계산하고, 이후에는 각 스탯별로 전투력을 계산하는 메서드들을 호출하여 사용
+
+        public float combatPower = 0;
+        public float combatPowerAttack = 0;
+        public float combatPowerHealth = 0;
+        public float combatPowerDefense = 0;
+        public float combatPowerCritical = 0;
+        public float combatPowerSpeed = 0;
+
+        /// <summary>
+        /// 초기화 후에 캐릭터의 모든 전투력을 계산하는 메소드, 최초에 전투력 산정을 위해서 사용
+        /// </summary>
+        /// <param name="characterData"></param>
+        public void AllCPCalculate(CharacterData characterData)
+        {
+            combatPowerAttack += CPAttack(characterData);
+            combatPowerHealth += CPHealth(characterData);
+            combatPowerDefense += CPDefense(characterData);
+            combatPowerCritical += CPCritical(characterData);
+            combatPowerSpeed += CPSpeed(characterData);
+            combatPower = combatPowerAttack + combatPowerHealth + combatPowerDefense + combatPowerCritical + combatPowerSpeed;
+        }
+
+        #region 스탯별 각 전투력 산정 메소드들
+        /// <summary>
+        /// 초기화 후 전투력 산정 공격력 부분을 계산하는 메소드
+        /// </summary>
+        /// <param name="characterData"></param>
+        /// <returns></returns>
+        public float CPAttack(CharacterData characterData)
+        {
+            characterData.attack = 0f;
+
+            switch (characterData.role)
+            {
+                case Role.Captain:
+                    return characterData.attack * 1.5f; 
+                case Role.Boatswain:
+                    return characterData.attack * 0.2f; 
+                case Role.Sailor:
+                    return characterData.attack * 1.2f;
+                case Role.Cook:
+                    return characterData.attack;
+                default:
+                    Debug.Log("캐릭터의 역할이 없습니다. 0을 반환합니다. (전투력 산출 공격력 부분)");
+                    return 0f; 
+            }
+        }
+
+        /// <summary>
+        /// 초기화 후 전투력 산정 체력 부분을 계산하는 메소드
+        /// </summary>
+        /// <param name="characterData"></param>
+        /// <returns></returns>
+        public float CPHealth(CharacterData characterData)
+        {
+            characterData.health = 0f;
+
+            switch (characterData.role)
+            {   case Role.Captain:
+                    return characterData.health * 0.3f; 
+                case Role.Boatswain:
+                    return characterData.health * 0.4f; 
+                case Role.Sailor:
+                    return characterData.health * 0.3f;
+                case Role.Cook:
+                    return characterData.health * 0.4f;
+                default:
+                    Debug.Log("캐릭터의 역할이 없습니다. 0을 반환합니다. (전투력 산출 체력 부분)");
+                    return 0f;
+            }
+        }
+
+        /// <summary>
+        /// 초기화 후 전투력 산정 방어력 부분을 계산하는 메소드
+        /// </summary>
+        /// <param name="characterData"></param>
+        /// <returns></returns>
+        public float CPDefense(CharacterData characterData)
+        {
+            characterData.defense = 0f;
+
+            switch (characterData.role)
+            {
+                case Role.Captain:
+                    return characterData.defense * 0.3f; 
+                case Role.Boatswain:
+                    return characterData.defense * 0.4f; 
+                case Role.Sailor:
+                    return characterData.defense * 0.2f;
+                case Role.Cook:
+                    return characterData.defense * 0.3f;
+                default:
+                    Debug.Log("캐릭터의 역할이 없습니다. 0을 반환합니다. (전투력 산출 방어력 부분)");
+                    return 0f;
+            }
+        }
+
+        /// <summary>
+        /// 초기화 후 전투력 산정 치명타(치명타 확률 및 치명타 데미지) 부분을 계산하는 메소드
+        /// </summary>
+        /// <param name="characterData"></param>
+        /// <returns></returns>
+        public float CPCritical(CharacterData characterData)
+        {
+            characterData.criticalChance = 0f;
+
+            switch (characterData.role)
+            {
+                case Role.Captain:
+                    return (characterData.criticalChance * characterData.criticalDamage / 200) * 0.8f; 
+                case Role.Boatswain:
+                    return (characterData.criticalChance * characterData.criticalDamage / 200) * 0.1f; 
+                case Role.Sailor:
+                    return (characterData.criticalChance * characterData.criticalDamage / 200) * 0.8f;
+                case Role.Cook:
+                    return (characterData.criticalChance * characterData.criticalDamage / 200) * 0.3f;
+                default:
+                    Debug.Log("캐릭터의 역할이 없습니다. 0을 반환합니다. (전투력 산출 치명타 확률 부분)");
+                    return 0f;
+            }
+
+        }
+
+        /// <summary>
+        /// 초기화 후 전투력 산정 속도 부분을 계산하는 메소드
+        /// </summary>
+        /// <param name="characterData"></param>
+        /// <returns></returns>
+        public float CPSpeed(CharacterData characterData)
+        {
+            characterData.speed = 0f;
+
+            switch (characterData.role)
+            {
+                case Role.Captain:
+                    return characterData.speed * 0.3f;
+                case Role.Boatswain:
+                    return characterData.speed * 0.4f;
+                case Role.Sailor:
+                    return characterData.speed * 0.2f;
+                case Role.Cook:
+                    return characterData.speed * 0.3f;
+                default:
+                    Debug.Log("캐릭터의 역할이 없습니다. 0을 반환합니다. (전투력 산출 속도 부분)");
+                    return 0f;
+            }
+        }
+        #endregion
+    }
+}
