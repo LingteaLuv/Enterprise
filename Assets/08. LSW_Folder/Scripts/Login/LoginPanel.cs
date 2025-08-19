cf689f26cfc342f3b1a1a23422ccc161c4462259
01stone01 <hagwhr2@gmail.com>
2025-08-20 08:04:03 +0900

    [Feat] 로그인 씬 플로우 구현
    로그인 씬에서 메인 씬으로 진입까지 플로우 구현

---
+++ Assets/08. LSW_Folder/Scripts/Login/LoginPanel.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginPanel : UIBase
+{
+    [SerializeField] private Button _googleLoginBtn;
+    [SerializeField] private Button _guestLoginBtn;
+
+    public Action OnLoginCompleted;
+
+    private void Start()
+    {
+        _googleLoginBtn.onClick.AddListener(async () => await OnTouchGoogleLoginBtn());
+        _guestLoginBtn.onClick.AddListener(async () => await OnTouchGuestLoginBtn());
+    }
+
+    private async Task OnTouchGoogleLoginBtn()
+    {
+        if (await AuthManager.Instance.GoogleLogin())
+        {
+            Debug.Log("구글 로그인 성공, Panel 변환");
+            OnLoginCompleted?.Invoke();
+        }
+    }
+    
+    private async Task OnTouchGuestLoginBtn()
+    {
+        Debug.Log("진입");
+        if (await AuthManager.Instance.GuestLogin())
+        {
+            Debug.Log("게스트 로그인 성공, Panel 변환");
+            OnLoginCompleted?.Invoke();
+        }
+    }
+}
