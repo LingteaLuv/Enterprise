f02bfc8e9543c8b5429f4a2f77741873e22d4962
01stone01 <hagwhr2@gmail.com>
2025-08-19 09:22:45 +0900

    [Feat] 팝업메세지 기능 스크립트 추가

---
+++ Assets/02. Scripts/PopupPanel.cs
@@ -0,0 +1,63 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupPanel : UIBase
+{
+    [SerializeField] private TMP_Text _messageText;
+    [SerializeField] private Button _leftButton;
+    [SerializeField] private TMP_Text _leftButtonText;
+    [SerializeField] private Button _rightButton;
+    [SerializeField] private TMP_Text _rightButtonText;
+    
+    public void SetShow(string message,
+        string leftText, Action onLeftClick,
+        string rightText, Action onRightClick)
+    {
+        gameObject.SetActive(true);
+        _messageText.text = message;
+
+        _leftButton.gameObject.SetActive(true);
+        _leftButtonText.text = leftText;
+        _leftButton.onClick.RemoveAllListeners();
+        _leftButton.onClick.AddListener(() => {
+            SetHide();
+            onLeftClick?.Invoke();
+        });
+
+        _rightButton.gameObject.SetActive(true);
+        _rightButtonText.text = rightText;
+        _rightButton.onClick.RemoveAllListeners();
+        _rightButton.onClick.AddListener(() => {
+            onRightClick?.Invoke();
+            SetHide();
+        });
+    }
+
+    public void SetShow(string message,
+        string leftText, Action onLeftClick)
+    {
+        gameObject.SetActive(true);
+        _messageText.text = message;
+
+        _leftButton.gameObject.SetActive(true);
+        _leftButtonText.text = leftText;
+        _leftButton.onClick.RemoveAllListeners();
+        _leftButton.onClick.AddListener(() => {
+            SetHide();
+            onLeftClick?.Invoke();
+        });
+
+        _rightButton.gameObject.SetActive(false);
+        _rightButton.onClick.RemoveAllListeners();
+    }
+
+    public override void SetHide()
+    {
+        base.SetHide();
+        _messageText.text = "";
+        _leftButton.onClick.RemoveAllListeners();
+        _rightButton.onClick.RemoveAllListeners();
+    }
+}
