278a115abd6f59621144fdb14dc77ff2be755c61
01stone01 <hagwhr2@gmail.com>
2025-08-26 00:48:14 +0900

    [Feat] PlayGames 로그인 기능 구현

--- Assets/08. LSW_Folder/Scripts/Login/LoginPanel.cs
+++ Assets/08. LSW_Folder/Scripts/Login/LoginPanel.cs
@@ -7,6 +7,7 @@ public class LoginPanel : UIBase
 {
     [SerializeField] private Button _googleLoginBtn;
     [SerializeField] private Button _guestLoginBtn;
+    [SerializeField] private Button _playGamesLoginBtn;
 
     public Action OnLoginCompleted;
 
@@ -14,6 +15,7 @@ public class LoginPanel : UIBase
     {
         _googleLoginBtn.onClick.AddListener(async () => await OnTouchGoogleLoginBtn());
         _guestLoginBtn.onClick.AddListener(async () => await OnTouchGuestLoginBtn());
+        _playGamesLoginBtn.onClick.AddListener(async () => await OnTouchPlayGamesLoginBtn());
     }
 
     private async Task OnTouchGoogleLoginBtn()
@@ -29,6 +31,16 @@ public class LoginPanel : UIBase
     {
         Debug.Log("진입");
         if (await AuthManager.Instance.GuestLogin())
+        {
+            Debug.Log("게스트 로그인 성공, Panel 변환");
+            OnLoginCompleted?.Invoke();
+        }
+    }
+    
+    private async Task OnTouchPlayGamesLoginBtn()
+    {
+        Debug.Log("진입");
+        if (await AuthManager.Instance.PlayGamesLogin())
         {
             Debug.Log("게스트 로그인 성공, Panel 변환");
             OnLoginCompleted?.Invoke();
