03a3b87f11aa80d993862b12c63a9d15a7cb40fb
01stone01 <hagwhr2@gmail.com>
2025-08-29 17:50:47 +0900

    [Feat] 인게임 메뉴 툴바 UI 연동

---
+++ Assets/08. LSW_Folder/Scripts/Game/Btn6Panel.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Btn6Panel : UIBase
+{
+    [SerializeField] private Button _exitBtn;
+    
+    public Action OnTouchedExitBtn;
+    
+    private void Start()
+    {
+        _exitBtn.onClick.AddListener(() =>
+        {
+            OnTouchedExitBtn?.Invoke();
+            gameObject.SetActive(false);
+        });
+        gameObject.SetActive(false);
+    }
+}
--- Assets/08. LSW_Folder/Scripts/Game/GameUIController.cs
+++ Assets/08. LSW_Folder/Scripts/Game/GameUIController.cs
@@ -1,13 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class GameUIController : UIController<GameUIController.GameUIType>
 {
-    [SerializeField] private Toggle _attendanceBtn;
+    [SerializeField] private Toggle _attendanceToggle;
+    [SerializeField] private Toggle _toolToggle;
+
     
     public enum GameUIType
     {
         AttendancePanel,
+        ToolPanel,
+        Btn1Panel,
+        Btn2Panel,
+        Btn3Panel,
+        Btn4Panel,
+        Btn5Panel,
+        Btn6Panel,
+        Btn7Panel,
+        Btn8Panel
     }
     
     private void Start()
@@ -21,11 +34,32 @@ public class GameUIController : UIController<GameUIController.GameUIType>
                     HideUI(GameUIType.AttendancePanel);
                 };
             }
+
+            if (ui is ToolPanel toolPanel)
+            {
+                toolPanel.OnButtonTouched += (index) =>
+                {
+                    for (int j = 0; j < _uiList.Count; j++)
+                    {
+                        HideUI((GameUIType)(j));
+                    }
+                    ShowUI((GameUIType)(index+2));
+                    _toolToggle.isOn = false;
+                };
+            }
         }
-        _attendanceBtn.onValueChanged.AddListener((isOn) =>
+        _attendanceToggle.onValueChanged.AddListener((isOn) =>
         {
-            if (isOn) HideUI(GameUIType.AttendancePanel);
+            if (!isOn) HideUI(GameUIType.AttendancePanel);
             else ShowUI(GameUIType.AttendancePanel);
         });
+        _toolToggle.onValueChanged.AddListener((isOn) =>
+        {
+            if (!isOn) HideUI(GameUIType.ToolPanel);
+            else ShowUI(GameUIType.ToolPanel);
+        });
+        
+        HideUI(GameUIType.AttendancePanel);
+        HideUI(GameUIType.ToolPanel);
     }
 }
