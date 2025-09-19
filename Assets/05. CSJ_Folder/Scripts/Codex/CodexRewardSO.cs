07c0bdfa745ba3544fe718488bd683923a05317f
seongjunC <163829433+seongjunC@users.noreply.github.com>
2025-09-19 20:36:57 +0900

    [feat] 도감 초안 작성
    도감에 대한 초안을 완성했습니다
    아직 연결되지 않은 기능들이 많지만 우선 기능과 UI 연결을 마쳤고 후에 레벨업/랭크업에 신호만 연결하면 됩니다.

---
+++ Assets/05. CSJ_Folder/Scripts/Codex/CodexRewardSO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace _05._CSJ_Folder.Scripts.Codex
+{
+    [CreateAssetMenu(menuName = "Codex/Reward")]
+    public class CodexRewardSO : ScriptableObject
+    {
+        [Serializable]
+        public struct CodexRewardEntry
+        {
+            public Sprite RewardIcon;
+            public CodexRewardType_Enum rewardType;
+            
+            public string RewardType => rewardType.ToString();
+            public int amount;
+        }
+        
+        public CodexRewardEntry[] RewardContents;
+        
+        public void AddReward(ICodexRewardGiver giver)
+        {
+            foreach (var reward in RewardContents)
+            {
+                giver.Give(reward);
+            }
+        }
+    }
+    
+    public interface ICodexRewardGiver
+    {
+        public void Give(CodexRewardSO.CodexRewardEntry entry);
+    }
+}
