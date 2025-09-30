82cda223c55f41050a779f66c4bf7726fb1603ff
01stone01 <hagwhr2@gmail.com>
2025-09-30 10:16:44 +0900

    [Feat] 닉네임 변경 기능 추가

---
+++ Assets/08. LSW_Folder/Scripts/NicknameChangePanel.cs
@@ -0,0 +1,87 @@
+using Firebase.Auth;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NicknameChangePanel : UIBase
+{
+    [SerializeField] private Button _closePopupButton;
+    [SerializeField] private Button _nicknameChangeButton;
+    [SerializeField] private TMP_InputField _nicknameField;
+    
+    private string _currentNickname;
+
+    public Action OnClickClosePopup;
+    public Action OnClickNicknameChange;
+    public Action OnClickNicknameChangeSuccess;
+
+    private void Start()
+    {
+        // 닉네임 글자 수 제한 (6글자)
+        _nicknameField.characterLimit = 8;
+
+        // 팝업 닫기 버튼
+        _closePopupButton.onClick.AddListener(() => OnClickClosePopup?.Invoke());
+        _nicknameChangeButton.onClick.AddListener(ChanegeNickname);
+    }
+
+    private void OnEnable()
+    {
+        FirebaseUser user = FirebaseManager.Auth.CurrentUser;
+
+        _currentNickname = user.DisplayName;
+
+        // placeholder 텍스트 = 유저 현재 닉네임
+        _nicknameField.placeholder.GetComponent<TMP_Text>().text = user.DisplayName;
+    }
+
+    /// <summary>
+    /// 안내 메세지 팝업을 띄우고 닫는 메서드
+    /// </summary>
+    /// <param name="message">팝업에 표시할 안내 메세지</param>
+    private void ShowPopup(string message)
+    {
+        //Debug.LogError(message);
+        PopManager.Instance.ShowOKPopup(message, "OK", () => PopManager.Instance.HidePopup());
+    }
+
+    private async void ChanegeNickname()
+    {
+        if (string.IsNullOrEmpty(_nicknameField.text.Trim()))
+        {
+            ShowPopup("닉네임을 입력해주세요.");
+            return;
+        }
+
+        // 닉네임 글자 수 체크
+        if (_nicknameField.characterLimit > 8)
+        {
+            ShowPopup("닉네임은 8글자 이내로 입력해 주세요.");
+            return;
+        }
+
+        // 기존 닉네임 일치 여부 체크
+        if (_currentNickname == _nicknameField.text)
+        {
+            Debug.LogError("동일 닉네임");
+            ShowPopup("기존에 사용 중인 닉네임과 동일합니다.\r\n다른 닉네임을 입력해 주세요.");
+            return;
+        }
+
+        //NicknameCheck();
+
+        // 닉네임 재설정 및 데이터베이스에 저장
+        await DatabaseManager.Instance.SetNickname(_nicknameField.text);
+        
+        ShowPopup("닉네임 변경 성공");
+
+        PopManager.Instance.ShowOKPopup("닉네임 변경 성공", "OK", () =>
+        {
+            PopManager.Instance.HidePopup();
+            
+            // 닉네임 변경 패널 비활성화
+            OnClickNicknameChangeSuccess?.Invoke();
+        });
+    }
+}
