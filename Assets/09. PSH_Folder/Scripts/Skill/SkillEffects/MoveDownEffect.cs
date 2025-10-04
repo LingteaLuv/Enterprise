ab6c530fa8fe1432a06cdcd98f558d98230a6bcf
xmasoo <xmasoo73@gmail.com>
2025-10-05 00:19:43 +0900

    [Feat] 내려오는 이펙트 스크립트

---
+++ Assets/09. PSH_Folder/Scripts/Skill/SkillEffects/MoveDownEffect.cs
@@ -0,0 +1,16 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class MoveDownEffect : MonoBehaviour, IMoveEffect
+{
+    public void Init(Transform target)
+    {
+    }
+
+    private void OnEnable()
+    {
+        Vector3 v = transform.position;
+        transform.position = transform.position + new Vector3(0f, 1f, 0f); 
+        gameObject.transform.DOMove(v, 1f);
+    }
+}
