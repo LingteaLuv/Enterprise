79ebd7812809fd1267390c6838ce9aaa7b961aed
xmasoo <xmasoo73@gmail.com>
2025-10-10 13:11:21 +0900

    [Fix] 캐릭터창 버튼 위에서 드래그할 때 클릭 방지

--- Assets/09. PSH_Folder/Scripts/Util/ButtonDragHandler.cs
+++ Assets/09. PSH_Folder/Scripts/Util/ButtonDragHandler.cs
@@ -5,16 +5,25 @@ using UnityEngine.UI;
 public class ButtonDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     private ScrollRect scrollRect;
+    private Button button;
+    private bool isDragging;
 
     void Awake()
     {
         // 자신의 부모 계층에서 ScrollRect 컴포넌트를 찾습니다.
         scrollRect = GetComponentInParent<ScrollRect>();
+        button = GetComponent<Button>();
     }
 
     // 드래그가 시작될 때 호출됩니다.
     public void OnBeginDrag(PointerEventData eventData)
     {
+        isDragging = true;
+
+        // 버튼 클릭 이벤트 차단
+        if (button != null)
+            button.interactable = false;
+
         if (scrollRect != null)
         {
             // 이벤트를 ScrollRect의 OnBeginDrag 메소드로 전달합니다.
@@ -35,10 +44,23 @@ public class ButtonDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler,
     // 드래그가 끝났을 때 호출됩니다.
     public void OnEndDrag(PointerEventData eventData)
     {
+        isDragging = false;
+
+        // 잠깐 비활성화 후 다시 활성화 (즉시 하면 Unity가 클릭으로 인식할 때가 있어서)
+        if (button != null)
+            StartCoroutine(EnableButtonNextFrame());
+
         if (scrollRect != null)
         {
             // 이벤트를 ScrollRect의 OnEndDrag 메소드로 전달합니다.
             scrollRect.OnEndDrag(eventData);
         }
+    }
+
+    private System.Collections.IEnumerator EnableButtonNextFrame()
+    {
+        yield return null;
+        if (button != null)
+            button.interactable = true;
     }
 }
