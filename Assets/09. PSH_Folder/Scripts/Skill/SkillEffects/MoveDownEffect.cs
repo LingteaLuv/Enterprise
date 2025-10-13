d2f0d597781fe5d04b49e39d7739e1c523b7ac5b
xmasoo <xmasoo73@gmail.com>
2025-10-13 15:44:25 +0900

    [Fix] 스킬 이펙트 초기화 문제 수정

--- Assets/09. PSH_Folder/Scripts/Skill/SkillEffects/MoveDownEffect.cs
+++ Assets/09. PSH_Folder/Scripts/Skill/SkillEffects/MoveDownEffect.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 
 public class MoveDownEffect : MonoBehaviour, IMoveEffect
@@ -9,8 +10,14 @@ public class MoveDownEffect : MonoBehaviour, IMoveEffect
 
     private void OnEnable()
     {
+        transform.DOKill(); // 기존 트윈 종료
+        StartCoroutine(StartMove());
+    }
+    private IEnumerator StartMove()
+    {
+        yield return null; // 한 프레임 대기 (위치 세팅 끝난 후)
         Vector3 v = transform.position;
-        transform.position = transform.position + new Vector3(0f, 1f, 0f); 
+        transform.position = transform.position + new Vector3(0f, 1f, 0f);
         gameObject.transform.DOMove(v, 1f);
     }
 }
